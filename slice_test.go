dcdf3b21c31a8d61ad41cd4187e109245f1b1d19
lpxxn <mi_duo@live.com>
2021-05-05 12:33:04 +0800

    feat: test

--- slice_test.go
+++ slice_test.go
@@ -1,6 +1,7 @@
 package fx_dig_adventure
 
 import (
+	"os"
 	"testing"
 
 	"go.uber.org/dig"
@@ -23,12 +24,12 @@ func TestGroup1(t *testing.T) {
 	if err := container.Provide(NewUser("jerry", 1), dig.Group("stu")); err != nil {
 		t.Fatal(err)
 	}
-	type UserParams struct {
+	type inParams struct {
 		dig.In
 
 		StudentList []*Student `group:"stu"`
 	}
-	Info := func(params UserParams) error {
+	Info := func(params inParams) error {
 		for _, u := range params.StudentList {
 			t.Log(u.Name, u.Age)
 		}
@@ -37,4 +38,48 @@ func TestGroup1(t *testing.T) {
 	if err := container.Invoke(Info); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestGroup2(t *testing.T) {
+	type Student struct {
+		Name string
+		Age  int
+	}
+	type Rep struct {
+		dig.Out
+		StudentList []*Student `group:"stu,flatten"`
+	}
+	NewUser := func(name string, age int) func() Rep {
+		return func() Rep {
+			r := Rep{}
+			r.StudentList = append(r.StudentList, &Student{
+				Name: name,
+				Age:  age,
+			})
+			return r
+		}
+	}
+
+	container := dig.New()
+	if err := container.Provide(NewUser("tom", 3)); err != nil {
+		t.Fatal(err)
+	}
+	if err := container.Provide(NewUser("jerry", 1)); err != nil {
+		t.Fatal(err)
+	}
+	type InParams struct {
+		dig.In
+
+		StudentList []*Student `group:"stu"`
+	}
+	Info := func(params InParams) error {
+		for _, u := range params.StudentList {
+			t.Log(u.Name, u.Age)
+		}
+		return nil
+	}
+	if err := container.Invoke(Info); err != nil {
+		t.Fatal(err)
+	}
+	dig.Visualize(container, os.Stdout)
 }
