11156eacf0402a89cded487aab42b8ab3761b9af
lpxxn <mi_duo@live.com>
2021-06-02 10:37:06 +0800

    feat: test fx

--- dig_test/name_provide_test.go
+++ dig_test/name_provide_test.go
@@ -1,9 +1,11 @@
 package dig_test
 
 import (
+	"context"
 	"testing"
 
 	"go.uber.org/dig"
+	"go.uber.org/fx"
 )
 
 func TestName1(t *testing.T) {
@@ -73,4 +75,106 @@ func TestName2(t *testing.T) {
 	if err := c.Invoke(inv1); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestName3(t *testing.T) {
+	type DSN struct {
+		Addr string
+	}
+	c := dig.New()
+
+	p1 := func() (*DSN, error) {
+		return &DSN{Addr: "primary DSN"}, nil
+	}
+	if err := c.Provide(p1, dig.Name("primary")); err != nil {
+		t.Fatal(err)
+	}
+	p2 := func() (*DSN, error) {
+		return &DSN{Addr: "secondary DSN"}, nil
+	}
+	if err := c.Provide(p2, dig.Name("secondary")); err != nil {
+		t.Fatal(err)
+	}
+
+	type DBInfo struct {
+		dig.In
+		PrimaryDSN   *DSN `name:"primary"`
+		SecondaryDSN *DSN `name:"secondary"`
+	}
+
+	if err := c.Provide(func(db DBInfo) string {
+		t.Log(db.PrimaryDSN)
+		t.Log(db.SecondaryDSN)
+		return "aaa"
+	}, dig.Name("str1")); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := c.Provide(func(db DBInfo) string {
+		t.Log(db.PrimaryDSN)
+		t.Log(db.SecondaryDSN)
+		return "bbb"
+	}, dig.Name("str2")); err != nil {
+		t.Fatal(err)
+	}
+
+	type StrInfo struct {
+		dig.In
+		Str1 string `name:"str1"`
+		Str2 string `name:"str2"`
+	}
+	if err := c.Invoke(func(s StrInfo) {
+		t.Log(s.Str1, "   ", s.Str2)
+	}); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestName4(t *testing.T) {
+	type DSN struct {
+		Addr string
+	}
+
+	p1 := func() (*DSN, error) {
+		return &DSN{Addr: "primary DSN"}, nil
+	}
+	p2 := func() (*DSN, error) {
+		return &DSN{Addr: "secondary DSN"}, nil
+	}
+
+	type DBInfo struct {
+		fx.In
+		PrimaryDSN   *DSN `name:"primary"`
+		SecondaryDSN *DSN `name:"secondary"`
+	}
+	type StrInfo struct {
+		fx.In
+		Str1 string `name:"str1"`
+		Str2 string `name:"str2"`
+	}
+
+	app := fx.New(fx.Provide(fx.Annotated{
+		Name:   "primary",
+		Target: p1,
+	}), fx.Provide(fx.Annotated{
+		Name:   "secondary",
+		Target: p2,
+	}), fx.Provide(fx.Annotated{
+		Name: "str1",
+		Target: func(db DBInfo) string {
+			t.Log(db.PrimaryDSN)
+			t.Log(db.SecondaryDSN)
+			return "aaa"
+		},
+	}), fx.Provide(fx.Annotated{
+		Name: "str2",
+		Target: func(db DBInfo) string {
+			t.Log(db.PrimaryDSN)
+			t.Log(db.SecondaryDSN)
+			return "bbb"
+		},
+	}), fx.Invoke(func(s StrInfo) {
+		t.Log(s.Str1, "   ", s.Str2)
+	}))
+	app.Start(context.Background())
 }
